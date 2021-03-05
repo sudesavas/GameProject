a79b88cade8007d3958999df8d51a94e449f2b63
SudeSva <sssvs@LAPTOP-UA03QDD4>
2021-03-06 00:53:06 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 2,
+                BirthYear = 1984,
+                FirstName = "SS",
+                LastName = "MİRACLE",
+                IdentityNumber = 123123
+            });
+                
+                    
+                    
+           
+        }
+    }
+}
